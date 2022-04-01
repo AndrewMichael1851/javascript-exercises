a07118daab9e95dc30724436b6eff4b0fc6b8e21
AndrewMichael1851 <andrewmichael1851@gmail.com>
2022-04-01 16:56:54 -0400

    Finished exercises 1 and 2.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(inputString, repeatNum) {
+    let testString = '';
 
+    if (repeatNum < 0) {
+        return 'ERROR'
+    } else if (inputString === '') {
+        return '';
+    } else {
+        for (var i = 0; i < repeatNum; i++) {
+            testString = testString + 'hey';
+        }
+        return testString;
+    }
 };
 
 // Do not edit below this line
