cbf00533eb90640d4281ef98f8e2280468192271
AndrewMichael1851 <andrewmichael1851@gmail.com>
2022-04-04 15:55:53 -0400

    Finished exercise 5.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(...inputArguments) {
 
+    if (Number.isInteger(inputArguments[0]) && inputArguments[0] > 0 && Number.isInteger(inputArguments[1]) && inputArguments[1] > 0) {
+        let highNumber = Math.max(inputArguments[0], inputArguments[1]);
+        let lowNumber = Math.min(inputArguments[0], inputArguments[1]);
+
+        let sumNumber = 0;
+
+        for (var i = lowNumber; i <= highNumber; i++) {
+            sumNumber = sumNumber + i;
+        }
+
+        return sumNumber;
+    } else {
+        return 'ERROR';
+    }
+    
 };
 
 // Do not edit below this line
