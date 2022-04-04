209da92129ea5c5f5b4e45e849887a921d61e9de
AndrewMichael1851 <andrewmichael1851@gmail.com>
2022-04-04 12:00:56 -0400

    Finished exercise 4.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(...inputArgument) {
+    let inputArray = inputArgument[0];
+    let newArray = [];
 
+    inputArray.forEach((item) => {
+        if (!inputArgument.includes(item)) {
+            newArray.push(item);
+        }
+    });
+
+    return newArray;
 };
 
 // Do not edit below this line
