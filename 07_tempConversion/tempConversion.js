3df637cb391161fe0a2c3bb2f250938daa99b727
AndrewMichael1851 <andrewmichael1851@gmail.com>
2022-04-04 16:14:49 -0400

    Finished exercise 7.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(inputFah) {
+    let newCel = (inputFah - 32) / 1.8;
+    newCel = Math.round(newCel * 10) / 10;
+    return newCel;
 };
 
-const ctof = function() {
-
+const ctof = function(inputCel) {
+    let newFah = (inputCel * 1.8) + 32;
+    newFah = Math.round(newFah * 10) / 10;
+    return newFah;
 };
 
 // Do not edit below this line
