1999a7a003d0035a2067ae42efd41af2e6c8c08c
AndrewMichael1851 <andrewmichael1851@gmail.com>
2022-04-15 14:03:02 -0400

    Finished exercise 10.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    index = parseInt(index);
+    if (index < 0) return 'ERROR';
+    if (index === 0) return 0;
 
+    let fibStartA = 0;
+    let fibStartB = 1;
+    for (let i = 0; i < index - 1; i++) {
+        const temp = fibStartB;
+        fibStartB = fibStartA + fibStartB;
+        fibStartA = temp;
+    };
+    return fibStartB;
 };
 
 // Do not edit below this line
