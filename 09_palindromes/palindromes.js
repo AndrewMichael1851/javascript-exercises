62e1e0d9f634d93e7af57e67a0d82cffd7a7bb7e
AndrewMichael1851 <andrewmichael1851@gmail.com>
2022-04-15 13:48:44 -0400

    Finished exercise 9.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    input = input.toLowerCase().replace(/[^a-z]/g, "");
+    let reverseString = [...input].reverse().join('');
+    if (reverseString === input) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
